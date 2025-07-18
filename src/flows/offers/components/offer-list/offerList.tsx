2a41f94b44e86f5868f9d3245a04f1989dbcdd82
unknown <vmfajar@gmail.com>
2025-07-18 12:13:15 +0200

    new error component

--- src/flows/offers/components/offer-list/offerList.tsx
+++ src/flows/offers/components/offer-list/offerList.tsx
@@ -1,3 +1,5 @@
+// Components
+import { ErrorComponent } from "../../../../components/error/error.component";
 // Hooks
 import { getOfferListHook } from "./hooks/getOfferList.hook";
 
@@ -7,8 +9,9 @@ const OfferList = () => {
     return <p>Cargando</p>;
   }
   if (error) {
-    return <p>Error</p>;
+    return <ErrorComponent />;
   }
+  // componente no resultados
   return (
     <div>
       <h1>Chollos</h1>
@@ -17,7 +20,7 @@ const OfferList = () => {
           <div key={offer.id}>
             <img src={offer.avatarUrl} alt={offer.merchantName} />
             <h2>{offer.title}</h2>
-            <p>{offer.description}</p>
+            <p>{offer.descri00ption}</p>
             <p>{offer.categories}</p>
             <p onClick={() => offer.city}>{offer.city}</p>
             <p>{offer.contact.phone}</p>
